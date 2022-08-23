db0994f5ec22ddb6d1dc7cd0ccf2e63e5b0aa5ce
Rafa <rafa@portillo.eu>
2022-08-23 08:37:03 +0200

    added NewTag

--- main.tf
+++ main.tf
@@ -3,12 +3,14 @@
 
 locals {
   name_suffix = "${var.app_name}-${var.environment}-${var.region}"
-  tags = {
-    Source          = "terraform cloud"
-    Env             = var.environment
-    CostCenter      = var.cost_center
-    ApplicationName = var.app_name
-  }
+  tags = merge(
+    { NewTag = "foobar" },
+    {
+      Source          = "terraform cloud"
+      Env             = var.environment
+      CostCenter      = var.cost_center
+      ApplicationName = var.app_name
+  })
 }
 
 module "static_website" {
